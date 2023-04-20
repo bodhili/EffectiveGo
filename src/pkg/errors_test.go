c7d77791d23574aecd5c735a738f8341d4675fbc
bodhili <present.h7s@gmail.com>
2023-04-20 11:10:07 +0800

    learn golang...

--- src/pkg/errors_test.go
+++ src/pkg/errors_test.go
@@ -1,23 +1,79 @@
 package pkg
 
 import (
-	"errors"
+	"bytes"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/gob"
+	"encoding/json"
+	"net/http"
 	"testing"
 )
 
-type DefinedError struct {
-	message string
-	code    int
-}
+func TestError(t *testing.T) {
+	cookie := make([]byte, 10)
+	_, _ = rand.Read(cookie)
 
-func (de *DefinedError) Error() string {
-	return de.message
-}
+	responseErr := NewResponseErr(
+		marshalParseErr,
+		"Method not allowed",
+		http.StatusText(http.StatusMethodNotAllowed),
+		http.StatusMethodNotAllowed,
+		map[string]string{
+			"cookie": base64.URLEncoding.EncodeToString(cookie),
+		})
 
-func TestNewError(t *testing.T) {
-	err := errors.New("test error")
+	errs := responseErr.Error()
+	if errs != "" {
+		t.Logf("marshal ret is:[%+v]", errs)
+	}
 
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(errs)
 	if err != nil {
-		t.Log(err)
+		panic(err)
+	}
+
+	t.Logf("buf bytes:%v", buf.Bytes())
+
+	t.Logf("marshal szie:[%d], buf size:[%d]", len(errs), buf.Len())
+}
+
+func NewResponseErr(code int, p string, err string, statusCode int, headers map[string]string) *ResponseError {
+	return &ResponseError{
+		Code:       code,
+		Err:        err,
+		Type:       p,
+		StatusCode: statusCode,
+		Headers:    headers,
+	}
+}
+
+type ResponseError struct {
+	Code       int               `json:"code"`
+	Err        string            `json:"err"`
+	Type       string            `json:"type"`
+	StatusCode int               `json:"status_code"`
+	Headers    map[string]string `json:"headers"`
+}
+
+const marshalParseErr = 1001
+
+func (re *ResponseError) Error() string {
+	ret, err := json.Marshal(*re)
+	if err != nil {
+		return statusText(re.Code)
+	}
+
+	return string(ret)
+}
+
+func statusText(code int) string {
+	switch code {
+	case marshalParseErr:
+		return "marshal parse is error"
+	default:
+		return "invalid status code"
 	}
 }
